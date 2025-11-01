f42c713076ac64813d20ecc3fc57de00155a7061
Reyes <00540924@uca.edu.sv>
2025-11-01 17:32:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-from modulos.venta import mostrar_venta  # ← minúscula aquí
+from modulos.Venta import mostrar_venta  # Importamos la función mostrar_venta del módulo Venta
 from modulos.login import login
 
+# Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
+    # Si la sesión está iniciada, mostrar el contenido de ventas
     mostrar_venta()
 else:
+    # Si la sesión no está iniciada, mostrar el login
     login()
 
