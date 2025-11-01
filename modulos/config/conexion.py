eb514821822734130b5204eeebdf485909c96eaf
Reyes <00540924@uca.edu.sv>
2025-10-31 23:20:08 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -4,10 +4,10 @@ from mysql.connector import Error
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host='be5bmntqvmjb45dbc68h-mysql.services.clever-cloud.com',
-            user='ufrsewvahgrdaghy',
-            password='UxDnJbPxibZaLwBC6Xt1',
-            database='be5bmntqvmjb45dbc68h',
+            host='boqicridrlrqfp7wqp0w-mysql.services.clever-cloud.com',
+            user='urccnva9ugksvokl',
+            password='rpt9b2B6Rqlj1PU5W0VI',
+            database='boqicridrlrqfp7wqp0w',
             port=3306
         )
         if conexion.is_connected():
